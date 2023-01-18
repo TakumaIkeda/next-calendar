352ce5f47f3a2c287f78ca98e7b31df77bb020c4
TakumaIkeda <i.taku0415@gmail.com>
2023-01-18 21:11:19 +0900

    ログインページにタイトルを設定

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,28 +1,34 @@
 import UnauthorizedLayout from "@/components/Layouts/UnauthorizedLayout";
 import { Button, Card, CardContent } from "@mui/material";
 import { NextPage } from "next";
+import Head from "next/head";
 
 const LoginPage: NextPage = () => {
   return (
-    <UnauthorizedLayout>
-      <div
-        style={{
-          height: "95vh",
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        <Card sx={{ minWidth: 275, minHeight: 250 }}>
-          <CardContent sx={{ display: "flex", justifyContent: "center" }}>
-            <Button variant="contained" color="error">
-              Googleでログイン
-            </Button>
-          </CardContent>
-        </Card>
-      </div>
-    </UnauthorizedLayout>
+    <>
+      <Head>
+        <title>ログイン|calendar app</title>
+      </Head>
+      <UnauthorizedLayout>
+        <div
+          style={{
+            height: "95vh",
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Card sx={{ minWidth: 275, minHeight: 250 }}>
+            <CardContent sx={{ display: "flex", justifyContent: "center" }}>
+              <Button variant="contained" color="error">
+                Googleでログイン
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </UnauthorizedLayout>
+    </>
   );
 };
 
